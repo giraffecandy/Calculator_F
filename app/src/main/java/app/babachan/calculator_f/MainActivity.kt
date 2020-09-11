87b0b137a75d200071c07a8c5901b991dcb1b499
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-11 13:21:14 +0900

    動くようになった

--- app/src/main/java/app/babachan/calculator_f/MainActivity.kt
+++ app/src/main/java/app/babachan/calculator_f/MainActivity.kt
@@ -9,106 +9,109 @@ class MainActivity : AppCompatActivity() {
 
     var answer: Int = 0
     var number: Int = 0
-    var operation: Int = 0
-    val textView: TextView = findViewById(R.id.textView)
+    private var operation: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView: TextView = findViewById(R.id.textView)
+
+
         textView.text = "0"
 
         zero.setOnClickListener {
             number = number * 10 + 0
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         one.setOnClickListener {
             number = number * 10 + 1
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         two.setOnClickListener {
             number = number * 10 + 2
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         three.setOnClickListener {
             number = number * 10 + 3
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         four.setOnClickListener {
             number = number * 10 + 4
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         five.setOnClickListener {
             number = number * 10 + 5
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         six.setOnClickListener {
             number = number * 10 + 6
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         seven.setOnClickListener {
             number = number * 10 + 7
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         eight.setOnClickListener {
             number = number * 10 + 8
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         nine.setOnClickListener {
             number = number * 10 + 9
-            textView.setText(number)
+            textView.text = number.toString()
         }
 
         fun calculate(){
             when (operation){
                 0 -> {answer = number}
-                1 -> {answer = answer + number}
-                2 -> {answer = answer - number}
-                3 -> {answer = answer * number}
-                4 -> {answer = answer / number}
+                1 -> { answer += number}
+                2 -> { answer -= number}
+                3 -> { answer *= number }
+                4 -> { answer /= number }
             }
             number = 0
         }
 
-        fun plus(){
+        plus.setOnClickListener{
             calculate()
             operation = 1
-            textView.text = ("+")
+            textView.text = "+"
         }
-        fun minus(){
+        minus.setOnClickListener{
             calculate()
             operation = 2
-            textView.text = ("-")
+            textView.text = "-"
         }
-        fun multiplication(){
+        multiplication.setOnClickListener{
             calculate()
             operation = 3
-            textView.text = ("×")
+            textView.text = "×"
         }
-        fun division(){
+        division.setOnClickListener{
             calculate()
             operation = 4
-            textView.text = ("÷")
+            textView.text = "÷"
         }
         fun allClear(){
             answer = 0
             number = 0
             operation = 0
         }
-        fun clear(){
+        clear.setOnClickListener{
             allClear()
             textView.text = answer.toString()
         }
-        fun equal(){
+
+        equal.setOnClickListener{
             calculate()
             textView.text = answer.toString()
             allClear()
