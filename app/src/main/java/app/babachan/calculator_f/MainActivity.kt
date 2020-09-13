50665dc9d7ec6ea113c711b15a4ac2ab92f2af4c
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-13 21:52:30 +0900

    ？

--- app/src/main/java/app/babachan/calculator_f/MainActivity.kt
+++ app/src/main/java/app/babachan/calculator_f/MainActivity.kt
@@ -1,9 +1,11 @@
 package app.babachan.calculator_f
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,101 +22,177 @@ class MainActivity : AppCompatActivity() {
 
         textView.text = "0"
 
+        fun yellow(){
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
+        }
+
+        val black = false
+            plus.isPressed = black
+
         zero.setOnClickListener {
             number = number * 10 + 0
             textView.text = number.toString()
+
+            if (zero.isPressed && black) {
+                plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+                minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+                multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            }
         }
 
         one.setOnClickListener {
             number = number * 10 + 1
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         two.setOnClickListener {
             number = number * 10 + 2
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         three.setOnClickListener {
             number = number * 10 + 3
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         four.setOnClickListener {
             number = number * 10 + 4
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         five.setOnClickListener {
             number = number * 10 + 5
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         six.setOnClickListener {
             number = number * 10 + 6
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         seven.setOnClickListener {
             number = number * 10 + 7
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         eight.setOnClickListener {
             number = number * 10 + 8
             textView.text = number.toString()
+
+            plus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            minus.setBackgroundColor(Color.parseColor("#ffeb3b"))
+            multiplication.setBackgroundColor(Color.parseColor("#ffeb3b"))
+
         }
 
         nine.setOnClickListener {
             number = number * 10 + 9
             textView.text = number.toString()
+
+            yellow()
         }
 
-        fun calculate(){
-            when (operation){
-                0 -> {answer = number}
-                1 -> { answer += number}
-                2 -> { answer -= number}
-                3 -> { answer *= number }
-                4 -> { answer /= number }
+        fun calculate() {
+            when (operation) {
+                0 -> {
+                    answer = number
+                }
+                1 -> {
+                    answer += number
+                }
+                2 -> {
+                    answer -= number
+                }
+                3 -> {
+                    answer *= number
+                }
+                4 -> {
+                    answer /= number
+                }
             }
             number = 0
         }
 
-        plus.setOnClickListener{
+        plus.setOnClickListener {
             calculate()
             operation = 1
             textView.text = "+"
+
         }
-        minus.setOnClickListener{
+        minus.setOnClickListener {
             calculate()
             operation = 2
             textView.text = "-"
         }
-        multiplication.setOnClickListener{
+        multiplication.setOnClickListener {
             calculate()
             operation = 3
             textView.text = "×"
         }
-        division.setOnClickListener{
-            calculate()
-            operation = 4
-            textView.text = "÷"
+
+        if (plus.isPressed || minus.isPressed || multiplication.isPressed) {
+
+            plus.setBackgroundColor(Color.parseColor("#e91e63"))
+            minus.setBackgroundColor(Color.parseColor("#e91e63"))
+            multiplication.setBackgroundColor(Color.parseColor("#e91e63"))
+
         }
-        fun allClear(){
+        fun allClear() {
             answer = 0
             number = 0
             operation = 0
         }
-        clear.setOnClickListener{
+
+        clear.setOnClickListener {
             allClear()
             textView.text = answer.toString()
         }
 
-        equal.setOnClickListener{
+        equal.setOnClickListener {
             calculate()
             textView.text = answer.toString()
             allClear()
         }
+
+
     }
 }
+
